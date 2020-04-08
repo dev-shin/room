e8c8d47adc721820700129e3d85d62eb374f889a
daveShin <hosin86@gmail.com>
2020-04-08 14:37:36 +0900

    no message

--- src/main/java/com/dsapp/room/domain/repository/RoomRepository.java
+++ src/main/java/com/dsapp/room/domain/repository/RoomRepository.java
@@ -18,7 +18,7 @@ public interface RoomRepository extends PagingAndSortingRepository<Room, Long>,
                 if(value.equals("Single")){
                     return root.roomFloor.trim().eq("1")
                             .and(root.buildingFloor.trim().eq("1"))
-                            //.and(root.isDeleted.isNull().or(root.isDeleted.eq(false)))
+                            .and(root.isDeleted.isNull().or(root.isDeleted.eq(false)))
                             .or(root.address.contains("건물전체"))
                             .or(root.address.contains("건물 전체"))
                             //.and(root.photos.length().gt(10))
